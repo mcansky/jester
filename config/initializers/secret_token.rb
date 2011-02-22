3a208841a2e14aecb969773a44ba8e7c8dc0d63e
mcansky <mcansky@arbousier.info>
2011-02-22 22:06:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jester::Application.config.secret_token = '633938773665fc56a8555ad4f9116270604dd3f41d6cf8ae60c880de570aa42ec8c1c25b83cc097e2afb0db147ef12b8d4dbdeb84c41b77f0c7213c3fa44fdb8'
